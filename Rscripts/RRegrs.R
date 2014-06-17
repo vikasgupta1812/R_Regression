21dca473ef0c86f2293a5b6742eef3113b77b79d
muntisa <muntisa@gmail.com>
2014-06-17 14:58:45 +0200

    Create RRegrs.R
    This is the main script file that will include different files/functions for all the data loading/filtering/scaling, feature selections, best regression models and specific statistics for the top model.

---
+++ Rscripts/RRegrs.R
@@ -0,0 +1,6 @@
+# RRegrs - R Regression Models
+#-------------------------------------------------------------
+# Best regression models for one dataset using R methods
+# Developed as APIs for OpenTox integration for nano-toxicity
+# NTUA and UM groups
+#-------------------------------------------------------------
